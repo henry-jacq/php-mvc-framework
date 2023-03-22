99b7c86d499cc1fd770018d7d47293063c6c9505
henry-jacq <henryeditz267@gmail.com>
2023-03-22 12:24:00 +0530

    feat: 404 page added

---
+++ views/_404.php
@@ -0,0 +1,12 @@
+<div class="d-flex align-items-center justify-content-center">
+    <div class="text-center py-5 mt-5">
+        <img src="https://cdni.iconscout.com/illustration/premium/thumb/404-error-message-3702341-3119133.png">
+        <!-- <h1 class="display-1 fw-bold">404</h1>
+        <p class="fs-3"> <span class="text-danger">Oops!</span> Page not found.</p>
+        <p class="lead">
+            The page you’re looking for doesn’t exist.
+        </p> -->
+        <br>
+        <a href="/" class="btn btn-primary">Go to Home</a>
+    </div>
+</div>
